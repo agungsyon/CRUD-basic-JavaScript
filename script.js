80da53c78ba66d16811dadf7ac3ac2d79c3ce089
Agung Syon <ahmatagung645@gmail.com>
2023-08-01 18:05:39 +0700

    skeleton done

---
+++ script.js
@@ -0,0 +1,23 @@
+const data = [
+    "Teknik Mesin",
+    "Teknik Sipil",
+    "Teknik Elektro",
+    "Teknik Komputer"
+]
+
+function tampil() {
+    
+}
+
+function tambah() {
+    
+}
+
+function edit() {
+    
+}
+
+function hapus() {
+    
+}
+
