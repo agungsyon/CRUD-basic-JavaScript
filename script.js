0a1898ff904efd3c7abd4a3283a3428854f63478
Agung Syon <ahmatagung645@gmail.com>
2023-08-01 19:06:42 +0700

    script success

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const data = [
-    "Teknik Mesin",
-    "Teknik Sipil",
-    "Teknik Elektro",
-    "Teknik Komputer"
+let data = [
+    "Syon",
+    "Syon Panjang"
 ]
 
 function tampil() {
-    
+    let tabel = document.getElementById('tabel');
+    tabel.innerHTML = "<tr><th>No</th><th>Name</th><th>Action</th></tr>";
+    for (let i = 0; i < data.length; i++) {
+        let btnEdit = "<button class='btn-edit' href='#' onclick='edit(" + i + ")'>Edit</button>";
+        let btnHapus = "<button class='btn-hapus' href='#' onclick='hapus(" + i + ")'>Hapus</button>";
+        j = i + 1;
+        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + data[i] + "</td><td>" + btnEdit + " " + btnHapus + "</tr>";
+    }
 }
 
 function tambah() {
-    
+    let input = document.querySelector("input[name=jurusan]");
+    data.push(input.value);
+    tampil();
+    input.value = "";
 }
 
-function edit() {
-    
+function edit(id) {
+    let baru = prompt("Edit", data[id]);
+    data[id] = baru;
+    tampil();
 }
 
-function hapus() {
-    
+function hapus(id) {
+    data.pop(id);
+    tampil();
 }
+
+tampil();
 
